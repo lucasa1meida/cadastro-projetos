e57e788d0ec10a2e964e6b56657e1aa8c446e8a5
Lucas Almeida <lucas.a1meida@hotmail.com>
2024-07-29 14:52:02 -0300

    Inicialização do projeto

---
+++ src/main/java/orla/cadastroprojetos/CadastroProjetosApplication.java
@@ -0,0 +1,13 @@
+package orla.cadastroprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProjetosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroProjetosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/orla/cadastroprojetos/CadastroProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package orla.cadastroprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProjetosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
